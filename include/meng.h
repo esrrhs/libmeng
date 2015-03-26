e37741dece317780f6d22b2554fccaf2514813d6
esrrhs <esrrhs@localhost>
2015-03-26 21:09:24 +0800

    sadg

--- include/meng.h
+++ include/meng.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MENG_VERSION "1.0"
+#define MENG_VERSION_NUM 100
+#define MENG_AUTHOR "esrrhs@163.com"
+
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,35 @@
+#include "../include/meng.h"
+#include <stdio.h>
+
+void func1(meng * m, void * arg)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("func1 %d\n", i);
+		meng_yield(m);
+	}
+}
+
+void func2(meng * m, void * arg)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("func2 %d\n", i);
+		meng_yield(m);
+	}
+}
+
 int main(int argc, const char * argv[])
 {
+	meng * m1 = meng_create(func1, 8 * 1024, 0);
+	meng * m2 = meng_create(func2, 8 * 1024, 0);
+	while (!meng_end(m1) && !meng_end(m2))
+	{
+		meng_run(m1);
+		meng_run(m2);
+	}
+	meng_delete(m1);
+	meng_delete(m2);
 	return 0;
 }
 
